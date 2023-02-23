812d5fdf1d0870e0ec08d52fdfc60640b93c4d53
Princekotadiya007 <princuuu77@gmail.com>
2023-02-23 13:57:35 +0530

    [prince]:add Create linked list

---
+++ LinkedListProgram/LinkedListProgram/LinkedList.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedListProgram
+{
+    public class LinkedList
+    {
+        public Node head;
+        //creating add method to add the data in linked list sequentially
+        public void Add(int data) //56  30  70
+        {
+            Node node = new Node(data); //56 
+            if(head == null)
+            {
+                this.head = node;   //56  30
+            }
+            else
+            {
+                Node temp = head;      // 56
+                while(temp.Next != null) // 56   
+                {
+                    temp = temp.Next;
+                }
+                temp.Next = node;   //30
+            }
+            Console.WriteLine("{0} Values Are Inserted", node.data);
+        }
+    }
+}
---
+++ LinkedListProgram/LinkedListProgram/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedListProgram
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList linkedList = new LinkedList();
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("Welcome to Link list program");
+                Console.WriteLine("\n1.Create linkedList");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        linkedList.Add(56);
+                        linkedList.Add(30);
+                        linkedList.Add(70);
+                        break;
+                    case 2:
+                        flag = false;
+                        break;
+                }
+            }
+        }
+    }
+}
